ab7a4989f100c045e0f3f34827792ea5ab624114
Sam-DeGuzman <sam.dg019@gmail.com>
2021-05-10 15:11:54 +0800

    Initial Commit

---
+++ lib/product.rb
@@ -0,0 +1,25 @@
+class Pastry
+    attr_accessor :name :code 
+
+def initialize(name, code)
+    @name = name
+    @code = code
+end
+
+
+class Vegemscroll < Pastry
+
+end
+
+
+class Blubmuf < Pastry
+
+end
+
+
+class Croissant < Pastry
+    
+end
+
+
+
