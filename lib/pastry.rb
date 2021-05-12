49608204d4e1a191dfebfd2ad44cb7ed363ad4bc
SamDg <sam.dg019@gmail.com>
2021-05-12 19:25:23 +0800

    Removes Unnecessary Comments

--- lib/pastry.rb
+++ lib/pastry.rb
@@ -37,21 +37,11 @@ def order(qty,code)
         @pack1 = {:qty => 2, :price => 9.95} 
         @pack2 = {:qty => 5, :price => 16.95} 
         @pack3 = {:qty => 8, :price => 24.95} 
-
-        puts qty
-        puts "Pack 1 quantity: #{@pack1[:qty]} with price of $#{@pack1[:price]}"
-        puts "Pack 2 quantity: #{@pack2[:qty]} with price of $#{@pack2[:price]}"
-        puts "Pack 3 quantity: #{@pack3[:qty]} with price of $#{@pack3[:price]}"
   
     elsif code == 'CF'
         @pack1 = {:qty => 3, :price => 5.95} 
         @pack2 = {:qty => 5, :price => 9.95} 
         @pack3 = {:qty => 9, :price => 16.99} 
-
-        puts qty
-        puts "Pack 1 quantity: #{@pack1[:qty]} with price of $#{@pack1[:price]}"
-        puts "Pack 2 quantity: #{@pack2[:qty]} with price of $#{@pack2[:price]}"
-        puts "Pack 3 quantity: #{@pack3[:qty]} with price of $#{@pack3[:price]}"
         
     end
 
